ce7fa1aa33d30fc5c0b95a677a39aebb54fcb277
ceidsmoe <ceidsmoe@uchicago.edu>
2017-01-05 00:23:09 -0600

    changed class to implment containers interface; added stubs

--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -1,21 +1,69 @@
 package uchidb;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author aelmore
  */
-public class HW0Runner {
+public class HW0Runner implements Containers<Integer, String> {
 
-	//TODO you likely will need to add member variable
+	private static HW0Runner instance = null;
+	
+	private HW0Runner() { }
 	
 	// This class is a factory for a singleton containers class.
 	// https://www.tutorialspoint.com/java/java_using_singleton.htm
 	public static Containers<Integer, String> getContainers() {
-		// TODO fix this function
+		if(instance == null) {
+	         instance = new HW0Runner();
+	      }
+	      return instance;
+	}
+
+	@Override
+	public Set<Integer> initSet(Integer[] tArray) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
+	public List<Integer> initList(Integer[] tArray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public Map<String, Integer> initEmptyMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void storeMap(Map<String, Integer> mapToStoreInClass) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean addToMap(String key, Integer value, boolean overwriteExistingKey) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Integer getValueFromMap(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getValueFromMap(String key, Integer defaultValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	public static void main(String[] args){
 		
 	}
